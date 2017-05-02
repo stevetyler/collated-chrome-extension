02b2b38aa1ceadf9930f3ab09fec9e52fb615bfa
Steve Tyler <stevetyler.uk@gmail.com>
2017-05-02 22:41:52 +0100

    started to implement context menu

--- contextMenu.js
+++ contextMenu.js
@@ -1,21 +1,22 @@
 //http://stackoverflow.com/questions/14245334/chrome-extension-sendmessage-from-background-to-content-script-doesnt-work
 
-// function onClickHandler(info, tab) {
-//     if (info.menuItemId == "line1"){
-//
-//       alert("You have selected: " + info.selectionText);
-//
-//       chrome.extension.sendMessage({action:'open_dialog_box'}, function(){});
-//
-//       alert("Req sent?");
-//
-//     }
-// }
-//
-//   chrome.contextMenus.onClicked.addListener(onClickHandler);
-//
-//   chrome.runtime.onInstalled.addListener(function() {
-//
-//   chrome.contextMenus.create({"id": "line1", "type": "normal", "title": "Save to Collated..", "contexts":["selection"]});
-//
-// });
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    "id": "saveTo",
+    "type": "link",
+    "title": "Save to Collated..",
+    "contexts":["link"],
+    "onclick": saveToCollated
+  });
+});
+
+//chrome.contextMenus.onClicked.addListener(saveToCollated);
+
+function saveToCollated(info, tab) {
+  if (info.menuItemId == "saveTo"){
+    alert("You have selected: " + info.selectionText);
+
+    // chrome.runtime.sendMessage({action:'open_dialog_box'}, function(){});
+    // alert("Req sent?");
+  }
+}
