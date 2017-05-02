94d3bcd63e4aec5a91e333a3cc10e7be0e39e77f
Steve Tyler <mail@steve-tyler.co.uk>
2017-05-03 00:05:04 +0100

    moved contextMenu code into pageEvent

--- contextMenu.js
+++ contextMenu.js
@@ -1,22 +1,28 @@
 //http://stackoverflow.com/questions/9418495/chrome-extension-why-i-have-this-error-in-console-of-the-background-page
+// http://stackoverflow.com/questions/21819021/chrome-extension-development-chrome-contextmenus-is-null
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.contextMenus.create({
-    "id": "saveTo",
-    "type": "link",
-    "title": "Save to Collated..",
-    "contexts":["link"],
-    "onclick": saveToCollated
-  });
-});
+var requestData = {"action": "createContextMenuItem"};
+//send request to background script
+chrome.extension.sendRequest(requestData);
+
+
+// chrome.runtime.onConnect.addListener(function() {
+//   chrome.contextMenus.create({
+//     "id": "saveTo",
+//     "type": "link",
+//     "title": "Save to Collated..",
+//     "contexts":["link"],
+//     "onclick": saveToCollated
+//   });
+// });
 
 //chrome.contextMenus.onClicked.addListener(saveToCollated);
 
-function saveToCollated(info, tab) {
-  if (info.menuItemId == "saveTo"){
-    alert("You have selected: " + info.selectionText);
-
-    // chrome.runtime.sendMessage({action:'open_dialog_box'}, function(){});
-    // alert("Req sent?");
-  }
-}
+// function saveToCollated(info, tab) {
+//   if (info.menuItemId == "saveTo"){
+//     alert("You have selected: " + info.selectionText);
+//
+//     chrome.runtime.sendMessage({action:'open_dialog_box'}, function(){});
+//     alert("Req sent?");
+//   }
+// }
